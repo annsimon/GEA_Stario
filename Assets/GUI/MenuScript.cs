a0584530e8cbf939b16b778be09e5f5c3101dc55
Anne-Lena Simon <annsimon@st.ovgu.de>
2014-06-18 22:31:28 +0200

    added splashscreen and menu, moved 1st level to scenes folder

---
+++ Assets/GUI/MenuScript.cs
@@ -0,0 +1,173 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	
+	public int levelID;
+
+	public GUIStyle background;
+	public GUIStyle backdrop;
+
+	public GUIStyle imageMain;
+	public GUIStyle imageSettings;
+	public GUIStyle imageCredits;
+
+	public GUIStyle text;
+
+	bool optionsMenuActive = false;
+	bool creditsMenuActive = false;
+
+	int buttonHeight = 35;
+	int buttonOffset = 5;
+	int headerOffset = 40;
+	int boxHeight;
+
+	// Use this for initialization
+	void Start () {
+		if(SplashScreenScript.menuActive)
+			Time.timeScale = 0.0f;
+		else
+			Time.timeScale = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// open menu on esc
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			SplashScreenScript.menuActive = true;
+			Time.timeScale = 0.0f;
+		}
+	}
+
+	// Gui stuff is done here
+	void OnGUI () {
+		if (!SplashScreenScript.menuActive)
+			return;
+		// Determine the screen size
+		int h = Screen.height;
+		int w = Screen.width;
+		int hc = h / 2;
+		int wc = w / 2;
+
+		// geometry calculations
+		int numButtons = 6;
+		boxHeight = 58 + numButtons * buttonHeight + (numButtons - 1) * buttonOffset;
+
+		// backdrop
+		GUI.Box(new Rect(0,0,w,h), "", backdrop);
+
+		if (optionsMenuActive)
+			DrawOptionsMenus (w,h,wc,hc);
+		else if (creditsMenuActive)
+			DrawCreditsMenu (w,h,wc,hc);
+		else
+			DrawMainMenu (w,h,wc,hc);
+	}
+
+	void DrawMainMenu(int w, int h, int wc, int hc)
+	{
+		// background image
+		GUI.Box (new Rect (w - 256 - 30, h - 256 - 30, 256, 256), "", imageMain);
+		
+		// background
+		GUI.Box (new Rect (wc - 200, hc - boxHeight/2, 400, boxHeight), "Main Menu", background);
+
+		// button to start/continue the game
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 0*buttonHeight + 0*buttonOffset, 
+		      				      360, 35), "Continue")) {
+			SplashScreenScript.menuActive = false;
+			Time.timeScale = 1.0f;
+		}
+
+		// button to restart the current level
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 1*buttonHeight + 1*buttonOffset,
+		                          360, 35), "Restart Level")) {
+			SplashScreenScript.menuActive = false;
+			Application.LoadLevel(levelID);
+		}
+
+		// button to restart the entire game
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 2*buttonHeight + 2*buttonOffset,
+		                          360, 35), "New Game")) {
+			SplashScreenScript.menuActive = false;
+			Application.LoadLevel(1);
+		}
+
+		// button to enter the options menu (and control reference)
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 3*buttonHeight + 3*buttonOffset,
+		                          360, 35), "Settings")) {
+			optionsMenuActive = true;
+		}
+
+		// button to display the credits
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 4*buttonHeight + 4*buttonOffset,
+		                          360, 35), "Credits")) {
+			creditsMenuActive = true;
+		}
+
+		// button to quit the game
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 5*buttonHeight + 5*buttonOffset,
+		                          360, 35), "Quit")) {
+				Application.Quit ();
+		}
+	}
+
+	void DrawOptionsMenus(int w, int h, int wc, int hc)
+	{
+		// controls text
+		string controls = "Move Stario: W, A, S, D \n" +
+						"Make Stario jump: Space \n" +
+						"Move Browser: - \n" +
+						"do w/e else is missing";
+		// background image
+		GUI.Box (new Rect (w - 256 - 30, h - 256 - 30, 256, 256), "", imageSettings);
+		
+		// background
+		GUI.Box (new Rect (wc - 200, hc - boxHeight/2, 400, boxHeight), "Settings", background);
+
+		// Text
+		GUI.Box (new Rect (wc - 200 + 40, hc - boxHeight/2 + 40, 400, boxHeight), controls, text);
+		
+		// button to start the game
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 5*buttonHeight + 5*buttonOffset,
+		                          360, 35), "Back")) {
+			optionsMenuActive = false;
+		}
+	}
+
+	void DrawCreditsMenu(int w, int h, int wc, int hc)
+	{
+		// credit text
+		string credits = "Prototype created for the lecture \n" +
+						 "Game Engine Architecture by \n" +
+						 "a \n" +
+						 "b \n" +
+						 "c \n" +
+						 "d \n" +
+						 "e \n \n \n"+
+						 "Supported by Acagamics (or something like that...)";
+
+		// background image
+		GUI.Box (new Rect (w - 256 - 30, h - 256 - 30, 256, 256), "", imageCredits);
+		
+		// background
+		GUI.Box (new Rect (wc - 200, hc - boxHeight/2, 400, boxHeight), "Credits", background);
+
+		// Text
+		GUI.Box (new Rect (wc - 200 + 40, hc - boxHeight/2 + 40, 400, boxHeight), credits, text);
+		
+		// button to start the game
+		if (GUI.Button (new Rect (wc - 180, 
+		                          hc - boxHeight/2 + headerOffset + 5*buttonHeight + 5*buttonOffset,
+		                          360, 35), "Back")) {
+			creditsMenuActive = false;
+		}
+	}
+}
---
+++ Assets/GUI/SplashScreenScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenScript : MonoBehaviour {
+	
+	public GUIStyle backdrop;
+	public GUIStyle image;
+
+	public static bool menuActive = true;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(StartGame(3.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// Gui stuff is done here
+	void OnGUI () {
+		// Determine the screen size
+		int h = Screen.height;
+		int w = Screen.width;
+		int hc = h / 2;
+		int wc = w / 2;
+		
+		// backdrop
+		GUI.Box(new Rect(0,0,w,h), "", backdrop);
+		
+		// splash screen image
+		GUI.Box(new Rect(w-256-30,h-256-30,256,256), "", image);
+	}
+
+	IEnumerator StartGame(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		Application.LoadLevel(1);
+	}
+}
