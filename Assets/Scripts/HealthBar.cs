a547011a2ec59db6955c4b73bcd086ea13d879dd
S.Rohde <sebastianrohde91@gmx.de>
2014-06-22 14:30:21 +0200

    Added Healthbar-Script + Heart-Images to use. Atm it's using hearts which are drawn at the upper left corner and exchanged with empty heart containers if damaged.
    HealthBar.cs has to be added to the Player & textures have to be added.
    
    Changed Raindrop.cs so that if raindrops collide with player, the health gets reduced by 1. (See bug below)
    
    !!! Bug !!!
    Still have a "bug":
    Health gets randomly reduced by 2 instead of 1 if hit by raindrops. Mostly happens if player stands right in front of raincloud.
    Maybe "double collision" (???) with player -> -2 health instead of -1.

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	private int maxHealth = 4; 
+	public int curHealth = 4; 
+	private float healthBarLength;
+	public Texture2D heart;
+	public Texture2D emptyHeart;
+	// Use this for initialization
+	void Start () {
+		healthBarLength = Screen.width / 4; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		adjustCurHealth (0) ; 
+	
+	}
+
+	void OnGUI () {
+
+		if (curHealth < 4) {
+			if (curHealth < 3) {
+				if (curHealth < 2) {
+					if (curHealth < 1) {
+						GUI.DrawTexture (new Rect (0, 0, 50, 50), emptyHeart);
+						GUI.DrawTexture (new Rect (50, 0, 50, 50), emptyHeart);
+						GUI.DrawTexture (new Rect (100, 0, 50, 50), emptyHeart);
+						GUI.DrawTexture (new Rect (150, 0, 50, 50), emptyHeart);
+						GUI.contentColor = Color.red;
+						GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, Screen.width - 20, Screen.height - 50), "Game Over");
+					}else{
+						GUI.DrawTexture (new Rect (0, 0, 50, 50), heart);
+						GUI.DrawTexture (new Rect (50, 0, 50, 50), emptyHeart);
+						GUI.DrawTexture (new Rect (100, 0, 50, 50), emptyHeart);
+						GUI.DrawTexture (new Rect (150, 0, 50, 50), emptyHeart);
+					}
+				}else{
+					GUI.DrawTexture (new Rect (0, 0, 50, 50), heart);
+					GUI.DrawTexture (new Rect (50, 0, 50, 50), heart);
+					GUI.DrawTexture (new Rect (100, 0, 50, 50), emptyHeart);
+					GUI.DrawTexture (new Rect (150, 0, 50, 50), emptyHeart);
+				}
+			}else{
+				GUI.DrawTexture (new Rect (0, 0, 50, 50), heart);
+				GUI.DrawTexture (new Rect (50, 0, 50, 50), heart);
+				GUI.DrawTexture (new Rect (100, 0, 50, 50), heart);
+				GUI.DrawTexture (new Rect (150, 0, 50, 50), emptyHeart);
+			}
+		}else{
+			GUI.DrawTexture (new Rect (0, 0, 50, 50), heart);
+			GUI.DrawTexture (new Rect (50, 0, 50, 50), heart);
+			GUI.DrawTexture (new Rect (100, 0, 50, 50), heart);
+			GUI.DrawTexture (new Rect (150, 0, 50, 50), heart);
+				}
+
+		// code for healthbar instead of hearts
+		/*if (curHealth == 0) {
+			GUI.contentColor = Color.red;
+			GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, Screen.width - 20, Screen.height - 50), "Game Over");
+		} 
+		else 
+		{
+			GUI.backgroundColor = Color.red;
+			GUI.Box (new Rect (maxHealth, maxHealth, Screen.width / 4, 20),curHealth+"/"+maxHealth);
+			GUI.backgroundColor = Color.green;
+			GUI.Box (new Rect (maxHealth, maxHealth, healthBarLength, 20), " ");
+		}*/
+		
+	} 
+
+	public void adjustCurHealth (int adj) {
+		
+		curHealth += adj;
+		
+		if(curHealth < 0)
+			curHealth = 0;
+		
+		if(curHealth > maxHealth)
+			curHealth = maxHealth;
+		
+		if(maxHealth < 1)
+			maxHealth = 1;
+		
+		healthBarLength = (Screen.width / 4) * (curHealth / (float)maxHealth);
+		
+	} 
+}
